a8f38e0ab42d49c81b6f6cbf42b1707536aa3c9b
siraj-eng <wahhajsiraj16@gmail.com>
2025-11-19 15:26:13 +0300

    Employee Insight system - Finishing on Employee class

--- Domain/Entities/Employee.cs
+++ Domain/Entities/Employee.cs
@@ -6,7 +6,46 @@ using System.Threading.Tasks;
 
 namespace Employee_Insight_System.Domain.Entities
 {
-    internal class Employee
+    public class Employee
     {
+        //Immutable Identity
+        public int EmployeeId { get; private set; }
+
+        //Core Profile details
+        public string EmployeeName {  get; private set; }
+        public int EmployeeAge { get; private set; }
+        public string EmployeePhone { get; private set; }
+
+        //--Additional properties--
+        public string Department {  get; private set; }
+        public DateTime DateHired { get; private set; }
+        public bool IsActive { get; private set; }
+        
+        //Constructor with validation
+        public Employee(int employeeId, string employeeName, 
+                        int employeeAge, string employeePhone,
+                        string department, DateTime dateHired) 
+        { 
+            if(employeeId <= 0)
+              throw new ArgumentException("Employee ID must be positive");
+            if (string.IsNullOrWhiteSpace(employeeName))
+                throw new ArgumentException("Employee name cannot be empty");
+            if (employeeAge < 18)
+                throw new ArgumentException("Employee must be atleast 18 years old");
+            if (string.IsNullOrWhiteSpace(employeePhone))
+                throw new ArgumentException("Employee phone number is required");
+
+            EmployeeId = employeeId;
+            EmployeeName = employeeName;
+            EmployeeAge = employeeAge;
+            EmployeePhone = employeePhone;
+            Department  = department;
+            DateHired = dateHired;
+            IsActive = true;
+
+            
+        }
+
+
     }
 }
