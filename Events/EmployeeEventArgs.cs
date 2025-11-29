96835923fa3411638220a6f23bf7777c94c3ad8d
siraj-eng <wahhajsiraj16@gmail.com>
2025-11-29 11:36:00 +0300

    Employee Insight system  - EmployeeEventArgs - Enterprise custom data type

--- Events/EmployeeEventArgs.cs
+++ Events/EmployeeEventArgs.cs
@@ -6,7 +6,38 @@ using System.Threading.Tasks;
 
 namespace Employee_Insight_System.Events
 {
-    internal class EmployeeEventArgs
+    public class EmployeeEventArgs : EventArgs
     {
+        // The employee entity involved in the event
+        public int EmployeeID { get; }
+        public string EmployeeName { get; }
+        public string Department {  get; }
+        public DateTime EventTimestamp { get; }
+
+        //Optional metadata for analytics
+        public string EventType { get; } // e.g "Onboarded" , "Terminated"
+        public string? Notes { get; } // FreeForm context
+
+        //Constructor
+        public EmployeeEventArgs(
+            int employeeID,
+            string employeeName,
+            string department,
+            string eventType,
+            string? notes = null)
+        {
+            EmployeeID = employeeID;
+            EmployeeName = employeeName;
+            Department = department;
+            EventType = eventType;
+            Notes = notes;
+
+            EventTimestamp = DateTime.Now;
+        }
+
+
+
+
+
     }
 }
